caa9b6c5629ae5783c733aebbbcf669d8ab5dde2
6666ev <2420851839@qq.com>
2022-03-13 21:35:54 +0800

    lyf

--- my_test.py
+++ my_test.py
@@ -1,10 +1,10 @@
 from rouge import FilesRouge
+import torch
 
-hyp_path="notebooks/gen_text_zm/decoded.txt"
-ref_path="notebooks/gen_text_zm/reference.txt"
-files_rouge = FilesRouge()
-scores = files_rouge.get_scores(hyp_path, ref_path)
-# print(scores)
-# or
-scores = files_rouge.get_scores(hyp_path, ref_path, avg=True)
-print(scores)
+bsz=32
+tgt_len=512
+mask = torch.full((tgt_len, tgt_len), 0.0)
+mask_cond = torch.arange(mask.size(-1))
+mask.masked_fill_(mask_cond < (mask_cond + 1).view(mask.size(-1), 1), 1.0)
+
+msk = mask[None, None, :, :].expand(bsz, 1, tgt_len, tgt_len)
